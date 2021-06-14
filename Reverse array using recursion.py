8a72d70e06c4f2fac2c8a4c6dcf5b26f112a8774
devSubho51347 <choudhurysubhadeep51347@gmail.com>
2021-06-14 21:47:07 +0530

    Version 2

--- Reverse array using recursion.py
+++ Reverse array using recursion.py
@@ -1,9 +1,9 @@
 def reverse_array(array,start, end):
     if start >= end:
-        return array
-    array = reverse_array(array,start = start + 1, end = end - 1)
+        return
+
     array[start], array[end] = array[end],array[start]
-    return array
+    reverse_array(array,start = start + 1, end = end - 1)
 
 
 
@@ -11,5 +11,7 @@ def reverse_array(array,start, end):
 
 array = [int(x) for x in input().split()]
 l = len(array)
+print(array)
+reverse_array(array,0,l - 1)
+print("reversed array:", array)
 
-print(reverse_array(array,0,l - 1))
