6824085045ee7a830f69dc6ee4107ad2d1e499b0
devSubho51347 <choudhurysubhadeep51347@gmail.com>
2021-06-21 17:43:26 +0530

    version 2 done using recursion

--- Cyclially rotate an array by one.py
+++ Cyclially rotate an array by one.py
@@ -1,16 +1,34 @@
-def cyclially_rotate(arr):
-    if len(arr) == 0:
-        return None
-    elif len(arr) == 1:
-        return arr
+# Version 1
+# Space complexity O(1) And Time Complexity O(n)
 
-    else:
-        for i in range(0,len(arr)-1,1):
-            arr[len(arr) - 1-i], arr[len(arr) - 2 - i] = arr[len(arr) - 2 - i],  arr[len(arr) - 1-i]
-        return arr
+# def cyclially_rotate(arr):
+#     if len(arr) == 0:
+#         return None
+#     elif len(arr) == 1:
+#         return arr
+#
+#     else:
+#         for i in range(0,len(arr)-1,1):
+#             arr[len(arr) - 1-i], arr[len(arr) - 2 - i] = arr[len(arr) - 2 - i],  arr[len(arr) - 1-i]
+#         return arr
+#
+#
+# li = [int(x) for x in input().split()]
+#
+# print("Cyclially rotated array is:", cyclially_rotate(li))
 
+# Version 2
+# Using recursive approach
+
+def reverse(arr,i):
+    if i == len(arr) - 1:
+        return arr
+    print(arr)
+
+    reverse(arr,i + 1)
+    arr[i+1],arr[i] = arr[i],arr[i+1]
+    print(arr)
+    return arr
 
 li = [int(x) for x in input().split()]
-
-print("Cyclially rotated array is:", cyclially_rotate(li))
-
+print(reverse(li,0))
